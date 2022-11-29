44d1bbe21c86853df235bd05cb186d5fc7858f0c
Dušan Reljić <reljicd@gmail.com>
2022-11-29 02:30:37 +0100

    Improved get_env and added option to use config file

--- commons/config/env_vars.py
+++ commons/config/env_vars.py
@@ -1,12 +1,26 @@
+import configparser
 import os
 
 from str2bool import str2bool
+
+config_file_path = os.getenv('CONFIG_FILE')
+if config_file_path:
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+else:
+    config = None
 
 
 def get_env(key: str, default: str) -> str:
     """ Returns default even if env var is empty string """
     env = os.getenv(key)
-    return env if env else default
+    if env:
+        return env
+
+    if config and key in config['DEFAULT'] and config['DEFAULT'][key]:
+        return config['DEFAULT'][key]
+
+    return default
 
 
 POSTGRES_HOST: str = get_env(key='POSTGRES_HOST', default='localhost')
